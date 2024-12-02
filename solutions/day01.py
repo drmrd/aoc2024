0302afc45b718878644f52b061fb1a5381c3d852
Dan Moore <hello@danielmoore.xyz>
2024-12-01 23:51:03 -0500

    DRY up Day 1 solution and add type hints

--- solutions/day01.py
+++ solutions/day01.py
@@ -2,23 +2,21 @@ from aoc2024 import location_ids, utilities
 
 
 def solution_to_part_one():
-    puzzle_inputs = [
-        [int(location_id) for location_id in line.split()]
-        for line in utilities.input_lines(day=1)
-    ]
-    list1 = [first_id for first_id, _ in puzzle_inputs]
-    list2 = [second_id for _, second_id in puzzle_inputs]
-    return location_ids.compare_lists(list1, list2)
+    return location_ids.compare_lists(*_location_id_lists())
 
 
 def solution_to_part_two():
+    return location_ids.similarity_score(*_location_id_lists())
+
+
+def _location_id_lists() -> tuple[list[int], list[int]]:
     puzzle_inputs = [
         [int(location_id) for location_id in line.split()]
         for line in utilities.input_lines(day=1)
     ]
     list1 = [first_id for first_id, _ in puzzle_inputs]
     list2 = [second_id for _, second_id in puzzle_inputs]
-    return location_ids.similarity_score(list1, list2)
+    return list1, list2
 
 
 if __name__ == '__main__':
