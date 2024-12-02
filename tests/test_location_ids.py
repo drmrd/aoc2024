da49a21c2507cd8bafd728afe3c841cb2ae6f17c
Dan Moore <hello@danielmoore.xyz>
2024-12-01 23:40:52 -0500

    Solve Day 1, Part 2

--- tests/test_location_ids.py
+++ tests/test_location_ids.py
@@ -1,5 +1,9 @@
-from aoc2024.location_ids import compare_lists
+from aoc2024.location_ids import compare_lists, similarity_score
 
 
-def test_day01_example():
+def test_compare_lists_day01_example():
     assert compare_lists([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]) == 11
+
+
+def test_similarity_score_day01_example():
+    assert similarity_score([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]) == 31
