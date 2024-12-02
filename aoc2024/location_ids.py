5eb83e6036dbbb8c1ae659ebbee00c0c10741563
Dan Moore <hello@danielmoore.xyz>
2024-12-01 23:40:52 -0500

    Solve Day 1, Part 2

--- aoc2024/location_ids.py
+++ aoc2024/location_ids.py
@@ -1,3 +1,11 @@
+from collections import Counter
+
+
+def similarity_score(list1, list2):
+    list2_occurrences = Counter(list2)
+    return sum(list1_id * list2_occurrences[list1_id] for list1_id in list1)
+
+
 def compare_lists(list1, list2):
     return sum(
         abs(id1 - id2)
--- solutions/day01.py
+++ solutions/day01.py
@@ -11,5 +11,16 @@ def solution_to_part_one():
     return location_ids.compare_lists(list1, list2)
 
 
+def solution_to_part_two():
+    puzzle_inputs = [
+        [int(location_id) for location_id in line.split()]
+        for line in utilities.input_lines(day=1)
+    ]
+    list1 = [first_id for first_id, _ in puzzle_inputs]
+    list2 = [second_id for _, second_id in puzzle_inputs]
+    return location_ids.similarity_score(list1, list2)
+
+
 if __name__ == '__main__':
     print('Solution to Part 1:', solution_to_part_one())
+    print('Solution to Part 2:', solution_to_part_two())
