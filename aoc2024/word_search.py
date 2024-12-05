4d33f189ac80a4cdd15cc2606b1aef80a3a78c42
Dan Moore <hello@danielmoore.xyz>
2024-12-04 20:04:20 -0500

    DRY up count functions

--- aoc2024/word_search.py
+++ aoc2024/word_search.py
@@ -7,29 +7,30 @@ def count(puzzle: str, words: Iterable[str]) -> int:
     return _count_in_rows(puzzle, words) + _count_in_columns(puzzle, words)
 
 
-def _count_in_rows(puzzle: str, words: Iterable[str]) -> int:
-    rows = puzzle.split('\n')
+def _count_in_strings(
+        strings: Iterable,
+        words: Iterable[str]
+) -> int:
     return sum(
         word == candidate
         for word_length, word_group in itertools.groupby(sorted(words, key=len), key=len)
         for word in word_group
-        for row in itertools.chain(rows, map(reversed, rows))
-        for candidate in _sliding_word(row, word_length)
+        for string in itertools.chain(strings, map(reversed, strings))
+        for candidate in _sliding_word(string, word_length)
     )
+
+
+def _count_in_rows(puzzle: str, words: Iterable[str]) -> int:
+    rows = puzzle.split('\n')
+    return _count_in_strings(rows, words)
 
 
 def _count_in_columns(puzzle: str, words: Iterable[str]) -> int:
     columns = list(zip(*puzzle.split('\n')))
-    return sum(
-        word == candidate
-        for word_length, word_group in itertools.groupby(sorted(words, key=len), key=len)
-        for word in word_group
-        for column in itertools.chain(columns, map(reversed, columns))
-        for candidate in _sliding_word(column, word_length)
-    )
+    return _count_in_strings(columns, words)
 
 
-def _sliding_word(text: str, word_length: int) -> Iterator:
+def _sliding_word(text, word_length: int) -> Iterator:
     word = collections.deque(itertools.islice(text, word_length - 1), maxlen=word_length)
     for x in text:
         word.append(x)
