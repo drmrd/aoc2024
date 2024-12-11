1823c219ad33349f71b5d2ef26ce7860748ebb03
Dan Moore <hello@danielmoore.xyz>
2024-12-10 19:36:16 -0500

    Solve Day 7, Part 2 recursively

--- aoc2024/calibrators.py
+++ aoc2024/calibrators.py
@@ -1,14 +1,37 @@
-from collections.abc import Iterable
+from collections.abc import Sequence
 
 
-def is_calibrated(test_value: int, operands: Iterable[int]) -> bool:
+def is_calibrated(
+        test_value: int,
+        operands: Sequence[int],
+        part2: bool
+) -> bool:
     *all_but_last, last = operands
     if not all_but_last:
         return test_value == last
     return (
-        is_calibrated(test_value - last, all_but_last)
+        is_calibrated(test_value - last, all_but_last, part2)
         or (
             (test_value % last == 0)
-            and is_calibrated(test_value // last, all_but_last)
+            and is_calibrated(test_value // last, all_but_last, part2)
+        )
+        or (
+            part2
+            and
+            test_value > 0
+            and
+            (
+                (test_value_string := str(test_value))[
+                    -len(last_string := str(last)):
+                ]
+            ) == last_string
+            and (
+                len(test_value_string) != len(last_string)
+            )
+            and is_calibrated(
+                int(test_value_string[:-len(last_string)]),
+                all_but_last,
+                part2
+            )
         )
     )
--- solutions/day07.py
+++ solutions/day07.py
@@ -13,12 +13,23 @@ def solve_part_one():
     return sum(
         test_value
         for test_value, operands in calibration_tests
-        if is_calibrated(test_value, operands)
+        if is_calibrated(test_value, operands, part2=False)
     )
 
 
 def solve_part_two():
-    return 'TBD'
+    calibration_tests = (
+        (int(test_value), tuple(int(operand) for operand in operands.split(' ')))
+        for test_value, operands in map(
+            lambda equation: equation.split(': '),
+            utilities.input_lines(day=7)
+        )
+    )
+    return sum(
+        test_value
+        for test_value, operands in calibration_tests
+        if is_calibrated(test_value, operands, part2=True)
+    )
 
 
 if __name__ == '__main__':
