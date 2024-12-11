23a38b22b65473a0db518271f0a82fa230a5a556
Dan Moore <hello@danielmoore.xyz>
2024-12-10 20:24:22 -0500

    Improve Day 7 solution readability
    This faster version of the solution is admittedly still not the most
    easily digested. Switching to a recursive function that builds up the
    test value from the operands rather than repeatedly undoing operations
    would have been easier to understand given Part 2.

--- tests/test_calibrators.py
+++ tests/test_calibrators.py
@@ -1,8 +1,19 @@
-from aoc2024.calibrators import is_calibrated
+import pytest
+
+from aoc2024.calibrators import (
+    is_calibrated,
+    potential_addition,
+    addition_partial_inverse,
+    potential_multiplication,
+    multiplication_partial_inverse,
+    potential_concatenation,
+    concatenation_partial_inverse
+)
 
 
-def test_is_calibrated_day7_part1_example():
-    example_calibration_tests = [
+@pytest.fixture
+def example_calibration_tests():
+    return [
         (190, (10, 19)),
         (3267, (81, 40, 27)),
         (83, (17, 5)),
@@ -14,10 +25,37 @@ def test_is_calibrated_day7_part1_example():
         (292, (11, 6, 16, 20))
     ]
 
+
+def test_is_calibrated_day7_part1_example(example_calibration_tests):
     sum_of_calibrated_test_values = sum(
         test_value
         for test_value, operands in example_calibration_tests
-        if is_calibrated(test_value, operands)
+        if is_calibrated(
+            test_value,
+            operands,
+            predicate_partial_inverse_pairs=[
+                (potential_addition, addition_partial_inverse),
+                (potential_multiplication, multiplication_partial_inverse)
+            ]
+        )
     )
 
     assert sum_of_calibrated_test_values == 3749
+
+
+def test_is_calibrated_day7_part2_example(example_calibration_tests):
+    sum_of_calibrated_test_values = sum(
+        test_value
+        for test_value, operands in example_calibration_tests
+        if is_calibrated(
+            test_value,
+            operands,
+            predicate_partial_inverse_pairs=[
+                (potential_addition, addition_partial_inverse),
+                (potential_multiplication, multiplication_partial_inverse),
+                (potential_concatenation, concatenation_partial_inverse)
+            ]
+        )
+    )
+
+    assert sum_of_calibrated_test_values == 11387
