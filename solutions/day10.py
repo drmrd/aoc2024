fd02cce49cd7a0942a42ab8ff8c7504be4f293b2
Dan Moore <hello@danielmoore.xyz>
2024-12-15 13:36:23 -0500

    Add a simple PyGame-based visualizer for Day 10, Part 1

--- solutions/day10.py
+++ solutions/day10.py
@@ -1,10 +1,54 @@
+import sys
 import timeit
 from collections import deque
 
+import pygame
+
 from aoc2024 import utilities
 
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
-def solve_part_one():
+VIRIDIS_RGB_PALETTE = [
+    (68, 1, 84),
+    (72, 40, 120),
+    (62, 73, 137),
+    (49, 104, 142),
+    (38, 130, 142),
+    (31, 158, 137),
+    (53, 183, 121),
+    (110, 206, 88),
+    (181, 222, 43),
+    (253, 231, 37)
+]
+
+VISUALIZER_COLORS = {
+    'active': {
+        'trailhead': {'color': (0, 255, 0), 'bg_color': (0, 255, 0)},
+        'default': {'color': (0, 255, 0), 'bg_color': (0, 255, 0)},
+        'peak': {'color': (0, 255, 0), 'bg_color': (0, 255, 0)}
+    },
+    'unvisited': {
+        'trailhead': {'color': (0, 255, 0), 'bg_color': (0, 0, 0)},
+        'default': {'color': (191, 191, 191), 'bg_color': (0, 0, 0)},
+        'peak': {'color': (255, 0, 0), 'bg_color': (0, 0, 0)}
+    },
+    'to_visit': {
+        'trailhead': {'color': (0, 255, 0), 'bg_color': (255, 95, 191)},
+        'default': {'color': (191, 191, 191), 'bg_color': (255, 95, 191)},
+        'peak': {'color': (255, 0, 0), 'bg_color': (255, 95, 191)}
+    },
+    'visited': {
+        'trailhead': {'color': (0, 255, 0), 'bg_color': (63, 63, 255)},
+        'default': {'color': (191, 191, 191), 'bg_color': (63, 63, 255)},
+        'peak': {'color': (255, 0, 0), 'bg_color': (63, 63, 255)}
+    }
+}
+
+
+def solve_part_one(visualize=False, fps=60):
     topographical_map = [
         list(map(int, line))
         for line in utilities.input_lines(day=10)
@@ -16,6 +60,24 @@ def solve_part_one():
         for column in range(map_shape[1])
         if topographical_map[row][column] == 0
     ]
+
+    if visualize:
+        visualizer = trail_search_visualizer(topographical_map, fps=fps)
+        topographical_map_data = [
+            [
+                {
+                    'height': topographical_map[row][column],
+                    'color': VISUALIZER_COLORS['unvisited']['default']['color'],
+                    'bg_color': VIRIDIS_RGB_PALETTE[topographical_map[row][column]]
+                }
+                for column in range(map_shape[1])
+            ]
+            for row in range(map_shape[0])
+        ]
+    else:
+        visualizer = infinite_coroutine()
+        topographical_map_data = None
+    next(visualizer)
 
     trailhead_scores = 0
     for trailhead in trailheads:
@@ -32,6 +94,14 @@ def solve_part_one():
                 )
                 if neighbor not in visited
             ))
+            update_visualizer_state(
+                topographical_map,
+                topographical_map_data,
+                node,
+                visited,
+                to_visit
+            )
+            visualizer.send(topographical_map_data)
         trailhead_scores += len({
             node for node in visited
             if topographical_map[node[0]][node[1]] == 9
@@ -58,8 +128,140 @@ def _neighbors_at_height(map, node, height, map_shape):
             yield neighbor
 
 
+def update_visualizer_state(
+        topographical_map,
+        topographical_map_data,
+        node,
+        visited,
+        to_visit
+):
+    if topographical_map_data is None:
+        return
+    node_type = height_to_node_type(topographical_map[node[0]][node[1]])
+    topographical_map_data[node[0]][node[1]]['color'] = (
+        VISUALIZER_COLORS['active'][node_type]['color']
+    )
+    topographical_map_data[node[0]][node[1]]['bg_color'] = (
+        VISUALIZER_COLORS['active'][node_type]['bg_color']
+    )
+    for row, column in to_visit:
+        node_type = height_to_node_type(topographical_map[row][column])
+        topographical_map_data[row][column]['color'] = (
+            VISUALIZER_COLORS['to_visit'][node_type]['color']
+        )
+        topographical_map_data[row][column]['bg_color'] = (
+            VISUALIZER_COLORS['to_visit'][node_type]['bg_color']
+        )
+    for row, column in visited:
+        node_type = height_to_node_type(topographical_map[row][column])
+        topographical_map_data[row][column]['color'] = (
+            VISUALIZER_COLORS['visited'][node_type]['color']
+        )
+        topographical_map_data[row][column]['bg_color'] = (
+            VISUALIZER_COLORS['visited'][node_type]['bg_color']
+        )
+
+
+def height_to_node_type(height):
+    match height:
+        case 0:
+            return 'trailhead'
+        case 9:
+            return 'peak'
+        case _:
+            return 'default'
+
+
+def trail_search_visualizer(topographical_map, fps=60):
+    map_shape = (len(topographical_map), len(topographical_map[0]))
+    block_size = 15
+
+    pygame.init()
+    screen = pygame.display.set_mode((
+        block_size * map_shape[1], block_size * map_shape[0]
+    ))
+    screen.fill(BLACK)
+
+    clock = pygame.time.Clock()
+
+    def handle_pygame_events():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def draw_grid(surface: pygame.Surface, topographical_map_data, block_size):
+        window_width, window_height = surface.get_size()
+        font = pygame.font.SysFont('Arial', block_size)
+        for x in range(0, window_width, block_size):
+            for y in range(0, window_height, block_size):
+                grid_rectangle_data = (
+                    topographical_map_data[y // block_size][x // block_size]
+                )
+                draw_grid_rectangle(
+                    x, y,
+                    surface=surface,
+                    text=str(grid_rectangle_data['height']),
+                    font=font,
+                    shape=(block_size, block_size),
+                    text_color=grid_rectangle_data['color'],
+                    background_color=grid_rectangle_data.get(
+                        'bg_color',
+                        VIRIDIS_RGB_PALETTE[grid_rectangle_data['height']]
+                    ),
+                    border_color=WHITE,
+                    border_width=5
+                )
+        pygame.display.update()
+
+    def draw_grid_rectangle(
+            x, y,
+            surface,
+            text,
+            font,
+            shape,
+            text_color,
+            background_color,
+            border_color,
+            border_width
+    ):
+        if isinstance(shape, int):
+            shape = (shape, shape)
+
+        pygame.draw.rect(
+            surface=surface,
+            color=border_color,
+            rect=(x, y, shape[0], shape[1]),
+            width=border_width
+        )
+        grid_rectangle = pygame.draw.rect(
+            surface=surface,
+            color=background_color,
+            rect=(x + 1, y + 1, shape[0] - 1, shape[1] - 1)
+        )
+        rendered_text = font.render(
+            text,
+            True,
+            text_color
+        )
+        text_rect = rendered_text.get_rect(center=grid_rectangle.center)
+        surface.blit(rendered_text, text_rect)
+
+    while True:
+        topographical_map_data = yield
+        draw_grid(screen, topographical_map_data, 15)
+        handle_pygame_events()
+        pygame.display.update()
+        clock.tick(fps)
+
+
+def infinite_coroutine(*args, **kwargs):
+    while True:
+        value = yield
+
+
 if __name__ == '__main__':
-    print('Solution to Part 1:', solve_part_one())
+    print('Solution to Part 1:', solve_part_one(visualize=True))
     print('Solution to Part 2:', solve_part_two())
 
     executions, repetitions = 100, 10
