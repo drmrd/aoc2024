5b8c6b1d24da3062ed25f23cdf65210ca4600164
Dan Moore <hello@danielmoore.xyz>
2024-12-15 23:50:34 -0500

    Add area function

--- tests/test_modern_garden.py
+++ tests/test_modern_garden.py
@@ -1,9 +1,9 @@
 import itertools
 
-from aoc2024.modern_garden import garden_regions
+from aoc2024.modern_garden import area, garden_regions
 
 
-def test_garden_regions_day12_part1_example1_regions():
+def test_garden_regions_day12_part1_example1():
     garden_plants = [
         ['A', 'A', 'A', 'A'],
         ['B', 'B', 'C', 'D'],
@@ -23,3 +23,15 @@ def test_garden_regions_day12_part1_example1_regions():
         frozenset({(3, 0), (3, 1), (3, 2)})
     }
     assert garden_regions(garden) == expected_regions
+
+
+def test_area_day12_part1_example1():
+    regions = [
+        frozenset({(0, 0), (0, 1), (0, 2), (0, 3)}),
+        frozenset({(1, 0), (1, 1), (2, 0), (2, 1)}),
+        frozenset({(1, 2), (2, 2), (2, 3), (3, 3)}),
+        frozenset({(1, 3)}),
+        frozenset({(3, 0), (3, 1), (3, 2)})
+    ]
+    for region in regions:
+        assert area(region) == len(region)
