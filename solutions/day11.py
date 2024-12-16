7779d1e58fa2699855be00516e90e8f92474db27
Dan Moore <hello@danielmoore.xyz>
2024-12-15 22:24:06 -0500

    Solve Day 11, Part 2

--- solutions/day11.py
+++ solutions/day11.py
@@ -1,18 +1,19 @@
 from aoc2024 import utilities
-from aoc2024.stone_simulator_2024 import StoneSimulator2024
+from aoc2024.stone_simulator_2024 import stones_after
 
 
 def solve_part_one():
-    stone_simulator = StoneSimulator2024(
-        map(int, next(utilities.input_lines(day=11)).split())
+    return sum(
+        stones_after(stone, 25)
+        for stone in map(int, next(utilities.input_lines(day=11)).split())
     )
-    for _ in range(25):
-        stone_simulator = stone_simulator.blink()
-    return len(tuple(stone_simulator))
 
 
 def solve_part_two():
-    return 'TBD'
+    return sum(
+        stones_after(stone, 75)
+        for stone in map(int, next(utilities.input_lines(day=11)).split())
+    )
 
 
 if __name__ == '__main__':
--- tests/test_stone_simulator_2024.py
+++ tests/test_stone_simulator_2024.py
@@ -1,15 +1,19 @@
-from aoc2024.stone_simulator_2024 import StoneSimulator2024
+from aoc2024.stone_simulator_2024 import stones_after
 
 
-def test_stone_simulator_day11_part1_example1():
-    stone_simulator = StoneSimulator2024([0, 1, 10, 99, 999])
-    assert list(stone_simulator.blink()) == [1, 2024, 1, 0, 9, 9, 2021976]
+def test_stones_after_day11_part1_example1():
+    total_stones = sum(
+        stones_after(stone, 1)
+        for stone in [0, 1, 10, 99, 999]
+    )
+    assert total_stones == len([1, 2024, 1, 0, 9, 9, 2021976])
 
 
-def test_stone_simulator_day11_part1_example2():
-    stone_simulator = StoneSimulator2024([125, 17])
+def test_stones_after_day11_part1_example2():
+    initial_stones = [125, 17]
 
     expected_blink_progression = [
+        initial_stones,
         [253000, 1, 7],
         [253, 0, 2024, 14168],
         [512072, 1, 20, 24, 28676032],
@@ -17,6 +21,9 @@ def test_stone_simulator_day11_part1_example2():
         [1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32],
         [2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2]
     ]
-    for expected_stones in expected_blink_progression:
-        stone_simulator = stone_simulator.blink()
-        assert list(stone_simulator) == expected_stones
+    for blinks, expected_stones in enumerate(expected_blink_progression):
+        total_stones = sum(
+            stones_after(initial_stone, blinks)
+            for initial_stone in initial_stones
+        )
+        assert total_stones == len(expected_stones)
