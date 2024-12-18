492121a2f053fde01bcd76fd959508c23466f402
Dan Moore <hello@danielmoore.xyz>
2024-12-17 21:41:25 -0500

    Add an input_grid utility

--- aoc2024/utilities.py
+++ aoc2024/utilities.py
@@ -6,6 +6,10 @@ REPO_ROOT = Path(__file__).parent.parent
 PUZZLE_INPUTS_DIRECTORY: Path = REPO_ROOT / 'puzzle_inputs'
 
 
+def input_grid(day: int) -> list[list[str]]:
+    return [input_line.split(',') for input_line in input_lines(day)]
+
+
 def input_lines(day: int) -> Generator[str]:
     with (PUZZLE_INPUTS_DIRECTORY / f'Day_{day:0>2}.txt').open() as input_file:
         for input_line in input_file:
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,9 +1,25 @@
 from aoc2024 import utilities
 
 
+def test_input_grid_returns_list_of_lists(monkeypatch, tmp_path):
+    monkeypatch.setattr(
+        utilities, 'PUZZLE_INPUTS_DIRECTORY', tmp_path / 'puzzle_inputs'
+    )
+    puzzle_inputs_dir = tmp_path / 'puzzle_inputs'
+    puzzle_inputs_dir.mkdir()
+    puzzle_day = 7
+    puzzle_grid = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']]
+    (puzzle_inputs_dir / f'Day_{puzzle_day:0>2}.txt').write_text(
+        '\n'.join(map(','.join, puzzle_grid))
+    )
+
+    assert utilities.input_grid(day=puzzle_day) == puzzle_grid
+
+
 def test_input_lines_returns_each_line_of_input_file_as_string(monkeypatch, tmp_path):
     monkeypatch.setattr(
-        utilities, 'PUZZLE_INPUTS_DIRECTORY', tmp_path / 'puzzle_inputs')
+        utilities, 'PUZZLE_INPUTS_DIRECTORY', tmp_path / 'puzzle_inputs'
+    )
     puzzle_inputs_dir = tmp_path / 'puzzle_inputs'
     puzzle_inputs_dir.mkdir()
     puzzle_day = 3
